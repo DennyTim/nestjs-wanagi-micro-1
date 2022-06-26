2cc43d2b8df369742417beb212f7ed74258b519b
Lucky <dtymokhin@livesoft.pro>
2022-06-26 22:43:05 +0300

    4-one-to-one -- initial changes -- added address

---
+++ src/users/address.entity.ts
@@ -0,0 +1,32 @@
+import { Expose } from "class-transformer";
+import {
+  Column,
+  Entity,
+  OneToOne,
+  PrimaryGeneratedColumn
+} from "typeorm";
+import { User } from "./user.entity";
+
+@Entity()
+export class Address {
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Column()
+  @Expose()
+  public street: string;
+
+  @Column()
+  @Expose()
+  public city: string;
+
+  @Column()
+  @Expose()
+  public country: string;
+
+  @OneToOne(
+    () => User,
+    (user: User) => user.address
+  )
+  public user: User;
+}
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -2,8 +2,11 @@ import { Expose } from "class-transformer";
 import {
   Column,
   Entity,
+  JoinColumn,
+  OneToOne,
   PrimaryGeneratedColumn
 } from "typeorm";
+import { Address } from "./address.entity";
 
 @Entity()
 export class User {
@@ -20,4 +23,21 @@ export class User {
 
   @Column()
   public password: string;
+
+  /**
+   * @eager - If we want our related entities always to be included
+   * instead of: ```this.addressRepository.find({ relations: ["user"] });```
+   * and separated route where we should create Address API
+   * @cascade - for saving Address entities via User API
+   * */
+  @OneToOne(
+    () => Address,
+    {
+      eager: true,
+      cascade: true
+    }
+  )
+  @JoinColumn()
+  @Expose()
+  public address: Address;
 }
